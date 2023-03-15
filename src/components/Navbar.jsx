7dac984ddf018a4fa37bd3f388036c587daf6fe2
Marius <marius.zilguzis@ktu.edu>
2023-03-15 12:49:54 +0200

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,39 @@
-export function Navbar(){
-    return(
+import "./styles/Navbar.css"
+export function Navbar() {
+    return (
         <>
+            <div className="navbar-box">
+                <div className="logo-box">
+                    <img src="imgs/logo-black.png" alt="Logo" />
+                </div>
+                <div className="info-box">
+                    <div className="icon-box">
+                        <img src="imgs/location.png" className="icon" alt="location" />
+                    </div>
+                    <div className="text-box">
+                        <p>A. Mickevičiaus st. 6,</p>
+                        <p>Kaunas, Lithuania</p>
+                    </div>
+                </div>
+                <div className="info-box">
+                    <div className="icon-box">
+                        <img src="imgs/clock.png" className="icon" alt="location" />
+                    </div>
+                    <div className="text-box">
+                        <p>8:00-17:00</p>
+                        <p>Saturday, Sunday : Closed</p>
+                    </div>
+                </div>
+                <div className="info-box">
+                    <div className="icon-box">
+                        <img src="imgs/telephone.png" className="icon" alt="location" />
+                    </div>
+                    <div className="text-box">
+                        <p>+370 640 27602</p>
+                        <p>info@cozyapartments.lt</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
