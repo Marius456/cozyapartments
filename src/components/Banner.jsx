aa48ec76c613001be19ac11ac2ddc4b2cc8fef2a
Marius456 <47567608+Marius456@users.noreply.github.com>
2023-03-17 11:45:31 +0200

    fix banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ export function Banner() {
             <div className="banner-box">
                 <div className="text">
                     <h2>Cozy Apartments - for your unforgettable stay in Kaunas</h2>
-                    <span>Please call or email us for more information</span>
+                    <span>Please call or email us for more information</span> 
                 </div>
             </div>
         </>
