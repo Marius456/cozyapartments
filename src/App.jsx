6a7ea7b38af37bcf9ffc9e5be6279d5b852dd8f3
Marius456 <47567608+Marius456@users.noreply.github.com>
2023-03-20 12:51:03 +0200

    added button banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { About } from "./components/About"
 import { Banner } from "./components/Banner"
+import { ButtonBanner } from "./components/ButtonBanner"
 import { Gallery } from "./components/Gallery"
 import { Navbar } from "./components/Navbar"
 import { NearbyPlaces } from "./components/NearbyPlaces"
@@ -15,6 +16,7 @@ function App() {
       <Banner/>
       <NearbyPlaces/>
       <Testimonials/>
+      <ButtonBanner/>
     </>
   )
 }
