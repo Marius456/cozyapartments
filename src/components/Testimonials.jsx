c06ee52bf7f709386a803e896d991d2391474b3e
Marius456 <47567608+Marius456@users.noreply.github.com>
2023-03-20 11:23:16 +0200

    fixed slider

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react"
 import testimonials from "../data/testimonials.json"
 import "./styles/Testimonials.css"
 
 export function Testimonials() {
+    const [testimonialId, setTestimonialId] = useState(0)
+
+    var t = setTimeout(() => {
+        if (testimonialId == testimonials.length - 1)
+            setTestimonialId(0);
+        else
+            setTestimonialId(testimonialId + 1);
+    }, 5000);
+
+
     return (
         <>
             <div className="testimonials-box">
@@ -10,18 +21,36 @@ export function Testimonials() {
                 </div>
                 <h2><strong>Words From</strong> Our Clients</h2>
                 <div className="testimonial-group">
-                    {testimonials.map((item) => (
-                        <div className="testimonial">
-                            <h3>{item.title}</h3>
-                            <div className="text">
-                                <span>{item.text}</span>
-                            </div>
-                            <div>
-                                <img src={item.imgUrl} alt="" />
-                                <div>{item.name}</div>
-                            </div>
+                    <div className="testimonial">
+                        <h3>{testimonials.at(testimonialId - 2).title}</h3>
+                        <div className="text">
+                            <span>{testimonials.at(testimonialId - 2).text}</span>
                         </div>
-                    ))}
+                        <div className="author">
+                            <img src={testimonials.at(testimonialId - 2).imgUrl} alt="portrait" />
+                            <div className="author-name">{testimonials.at(testimonialId - 2).name}</div>
+                        </div>
+                    </div>
+                    <div className="testimonial">
+                        <h3>{testimonials.at(testimonialId - 1).title}</h3>
+                        <div className="text">
+                            <span>{testimonials.at(testimonialId - 1).text}</span>
+                        </div>
+                        <div>
+                            <img src={testimonials.at(testimonialId - 1).imgUrl} alt="portrait" />
+                            <div>{testimonials.at(testimonialId - 1).name}</div>
+                        </div>
+                    </div>
+                    <div className="testimonial">
+                        <h3>{testimonials.at(testimonialId).title}</h3>
+                        <div className="text">
+                            <span>{testimonials.at(testimonialId).text}</span>
+                        </div>
+                        <div>
+                            <img src={testimonials.at(testimonialId).imgUrl} alt="portrait" />
+                            <div>{testimonials.at(testimonialId).name}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
